de9d12733f0435543da5e9b0031436ec01069e2d
wangdi <978453667@qq.com>
2019-07-28 18:47:47 +0800

    把vant配置好了可以应用于750px的模板了

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
   <div id="app">
     <h1>App----</h1>
     <van-button type="default">默认按钮</van-button>
+    <div class="test"></div>
     <router-view />
   </div>
 </template>
 
 <style lang="less">
-
+.test {
+  // 设计稿尺寸为750,除以2作为px值
+  width: 700px;
+  height: 200px;
+  background-color: yellow;
+}
 </style>
